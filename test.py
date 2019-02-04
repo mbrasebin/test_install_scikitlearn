f555dc6874a6788421ffa03a9fcceeacfe19c803
Mickaël Brasebin <mickael.brasebin@gmail.com>
2019-02-04 22:32:41 +0100

    Push

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
 import os
-
-try:
-    from pip._internal import main
-except:
-    plugin_dir=os.path.dirname(__file__)
-    getpipscript= os.path.join(plugin_dir, "get-pip.py")
-    import subprocess
-    subprocess.call("python3 get-pip.py --user", shell=True)
-    #exec(compile(open(getpipscript).read(), getpipscript, 'exec'))
-    from pip._internal import main
-    # just in case the included version is old
-    main(['install','--upgrade','--user','pip3'])
-    main(['freeze','--user','pip3'])
-    print("Try to install with pip3 directly")    
+  
     
 # For windows users
 try:
@@ -28,7 +15,6 @@ except:
     import subprocess
     print("Try to install with subprocess")
     subprocess.call(['pip3', 'install', '-U' ,'--user', 'scikit-learn'])
-    subprocess.call(['pip3', 'freeze', '-U' ,'--user'])
 
 from sklearn import datasets
 
