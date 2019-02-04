4e00e5ea9419d4acd697d2916b32bbd09abb67e0
Mickaël Brasebin <mickael.brasebin@gmail.com>
2019-02-04 22:27:20 +0100

    Adding log

--- test.py
+++ test.py
@@ -10,32 +10,26 @@ except:
     #exec(compile(open(getpipscript).read(), getpipscript, 'exec'))
     from pip._internal import main
     # just in case the included version is old
-    main(['install','--upgrade','--user','pip'])
-    main(['freeze','--user','pip'])
+    main(['install','--upgrade','--user','pip3'])
+    main(['freeze','--user','pip3'])
+    print("Try to install with pip3 directly")    
     
-    
-    
-
-try:
-    from sklearn import datasets
-except:
-    from pip._internal import main
-    main(['install','-U' ,'--user', 'scikit-learn'])
-
 # For windows users
 try:
     from sklearn import datasets
 except:
     import subprocess
     subprocess.call(['easy_install', '-m', 'scikit-learn'])
-    
+    print("Try to install with easy install")
 
 try:
     from sklearn import datasets
 except:
     import subprocess
-    subprocess.call(['pip', 'install', '-U' ,'--user', 'scikit-learn'])
-    
+    print("Try to install with subprocess")
+    subprocess.call(['pip3', 'install', '-U' ,'--user', 'scikit-learn'])
+    subprocess.call(['pip3', 'freeze', '-U' ,'--user'])
+
 from sklearn import datasets
 
 print("It is ok")
