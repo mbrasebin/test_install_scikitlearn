1554ebb91b1bc1b21841aa78f98063174f50bfd0
Mickaël Brasebin <mickael.brasebin@gmail.com>
2019-02-04 22:47:18 +0100

    Push

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
 import os
-  
-    
-# For windows users
+
 try:
-    from sklearn import datasets
+    from pip._internal import main
 except:
+    plugin_dir=os.path.dirname(__file__)
+    getpipscript= os.path.join(plugin_dir, "get-pip.py")
     import subprocess
-    subprocess.call(['easy_install', '-m', 'scikit-learn'])
-    print("Try to install with easy install")
+    subprocess.call("python3 get-pip.py --user", shell=True)
+    #exec(compile(open(getpipscript).read(), getpipscript, 'exec'))
+    from pip._internal import main
+    # just in case the included version is old
+    main(['install','--upgrade','-U','pip3'])
+    main(['freeze','--user','pip3'])
 
 try:
     from sklearn import datasets
 except:
     import subprocess
     print("Try to install with subprocess")
-    subprocess.call(['pip3', 'install', '--user', 'scikit-learn'])
+    subprocess.call(['pip3', 'install', '-U', 'scikit-learn'])
     subprocess.call(['pip3', 'list'])
 
 from sklearn import datasets
