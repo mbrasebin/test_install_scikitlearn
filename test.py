636819a7d6270376658152c9c5a50a4d1b588519
Mickaël Brasebin <mickael.brasebin@gmail.com>
2019-02-04 22:38:36 +0100

    add m

--- test.py
+++ test.py
@@ -14,7 +14,8 @@ try:
 except:
     import subprocess
     print("Try to install with subprocess")
-    subprocess.call(['pip3', 'install', '-m','-U' ,'--user', 'scikit-learn'])
+    subprocess.call(['pip3', 'install', '-m','-U' , 'scikit-learn'])
+    subprocess.call(['pip3', 'list'])
 
 from sklearn import datasets
 
